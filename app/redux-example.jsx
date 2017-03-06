d9a7dc270fbf47d3eb096669c647993e236b25c8
Denis Michailov <kamrad.reg@gmail.com>
2017-03-06 23:46:39 +1000

    Add subscribe and dev tools

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -14,13 +14,31 @@ var reducer = (state = {name: 'Anonymous'}, action) => {
             return state;
     }
 };
-var store = redux.createStore(reducer);
+var store = redux.createStore(reducer, redux.compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+// Subscribe to changes
+var unsubscribe = store.subscribe(() => {
+    var state = store.getState();
+    console.log('Name is', state.name);
+    document.getElementById('app').innerHTML = state.name;
+});
+
 var currentState = store.getState();
 console.log('currentState', currentState);
 
 store.dispatch({
     type: 'CHANGE_NAME',
-    name: 'Andrew'
+    name: 'Denis'
 });
 
-console.log('Name should be Andrew', store.getState());
+store.dispatch({
+    type: 'CHANGE_NAME',
+    name: 'Leo'
+});
+
+store.dispatch({
+    type: 'CHANGE_NAME',
+    name: 'Nastya'
+});
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -19,12 +19,24 @@ var reducer = (state = stateDefault, action) => {
     }
     return state;
 };
-var store = redux.createStore(reducer);
-console.log('currentState', store.getState());
+
+var store = redux.createStore(reducer, redux.compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+// Subscribe to changes
+store.subscribe(() => {
+    var state = store.getState();
+    document.getElementById('app').innerHTML = state.searchText;
+    console.log('Name is', state.searchText);
+});
 
 store.dispatch({
     type: 'CHANGE_SEARCH_TEXT',
-    searchText: 'New Text'
+    searchText: 'Find My Cat'
 });
 
-console.log('Search text should be New Text', store.getState());
+store.dispatch({
+    type: 'CHANGE_SEARCH_TEXT',
+    searchText: 'Find Something Else'
+});
