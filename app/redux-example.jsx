937cfb2c95c7046fb744c609742178476877de8f
Denis Michailov <kamrad.reg@gmail.com>
2017-03-26 00:56:05 +1000

    Add actions for adding/removing hobbies and movies

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,39 +1,118 @@
 var redux = require('redux');
 
-console.log('Starting redux example');
+console.log('--- Starting redux example ---');
 
-var reducer = (state = {name: 'Anonymous'}, action) => {
-    switch (action.type) {
-        case 'CHANGE_NAME':
-            return {
-                ...state,
-                name: action.name
-            };
-        default:
-            return state;
-    }
+var stateDefault = {
+  name: 'Anonymous',
+  hobbies: [],
+  movies: []
+}
+
+var nextHobbyId = 1;
+var nextMovieId = 1;
+var reducer = (state = stateDefault, action) => {
+  switch (action.type) {
+    case 'CHANGE_NAME':
+      return {
+        ...state,
+        name: action.name
+      };
+    case 'ADD_HOBBY':
+      return {
+        ...state,
+        hobbies: [
+          ...state.hobbies,
+          {
+            id: nextHobbyId++,
+            hobby: action.hobby
+          }
+        ]
+      };
+    case 'REMOVE_HOBBY':
+      return {
+        ...state,
+        hobbies: state.hobbies.filter((hobby) => {
+          return hobby.id !== action.id;
+        })
+      };
+    case 'ADD_MOVIE':
+      return {
+        ...state,
+        movies: [
+          ...state.movies,
+          {
+            id: nextMovieId++,
+            title: action.title,
+            genre: action.genre
+          }
+        ]
+      };
+    case 'REMOVE_MOVIE':
+      return {
+        ...state,
+        movies: state.movies.filter((movie) => {
+          return movie.id !== action.id;
+        })
+      }
+    default:
+      return state;
+  }
 };
 
 var store = redux.createStore(reducer, redux.compose(
-    window.devToolsExtension ? window.devToolsExtension() : f => f
+  window.devToolsExtension ? window.devToolsExtension() : f => f
 ));
 
 // Subscribe to changes
 var unsubscribe = store.subscribe(() => {
-    var state = store.getState();
-    console.log('Name is', state.name);
-    document.getElementById('app').innerHTML = state.name;
+  var state = store.getState();
+  console.log('Name is', state.name);
+  document.getElementById('app').innerHTML = state.name;
+  console.log('New state', store.getState());
 });
 
 var currentState = store.getState();
 console.log('currentState', currentState);
 
 store.dispatch({
-    type: 'CHANGE_NAME',
-    name: 'Denis'
+  type: 'CHANGE_NAME',
+  name: 'Denis'
+});
+
+// unsubscribe();
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Running'
 });
 
 store.dispatch({
-    type: 'CHANGE_NAME',
-    name: 'Nastya'
+  type: 'ADD_HOBBY',
+  hobby: 'Walking'
+});
+
+store.dispatch({
+  type: 'REMOVE_HOBBY',
+  id: 2
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Blade Runner',
+  genre: 'Fantastic'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Aliens',
+  genre: 'Fantastic'
+});
+
+store.dispatch({
+  type: 'REMOVE_MOVIE',
+  id: 2
+})
+
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'Nastya'
 });
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -1,46 +1,59 @@
 var redux = require('redux');
 
-console.log('Starting todo redux example');
+console.log('--- Starting todo redux example ---');
 
 var stateDefault = {
-    searchText: '',
-    showCompleted: false,
-    todos: []
+  searchText: '',
+  showCompleted: false,
+  todos: []
 };
 var reducer = (state = stateDefault, action) => {
-    switch(action.type) {
-        case 'CHANGE_SEARCH_TEXT':
-            return {
-                ...state,
-                searchText: action.searchText
-            };
-        default:
-            return state;
-    }
+  switch(action.type) {
+    case 'CHANGE_SEARCH_TEXT':
+    return {
+      ...state,
+      searchText: action.searchText
+    };
+    case 'CHANGE_SHOW_COMPLETED':
+    return {
+      ...state,
+      showCompleted: !state.showCompleted
+    };
+    default:
     return state;
+  }
+  return state;
 };
 
-var store = redux.createStore(reducer);
+// var store = redux.createStore(reducer);
+
+var store = redux.createStore(reducer, redux.compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
 console.log('currentState', store.getState());
 
-// var store = redux.createStore(reducer, redux.compose(
-//     window.devToolsExtension ? window.devToolsExtension() : f => f
-// ));
+// Subscribe to changes
+store.subscribe(() => {
+  var state = store.getState();
+  document.getElementById('app').innerHTML = state.searchText;
+  console.log('Name:', state.searchText );
+  console.log('Show completed:', state.showCompleted );
+});
 
+store.dispatch({
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'Find New Job'
+});
 
-// // Subscribe to changes
-// store.subscribe(() => {
-//     var state = store.getState();
-//     document.getElementById('app').innerHTML = state.searchText;
-//     console.log('Name is', state.searchText);
-// });
+store.dispatch({
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'Learn More About ngModules'
+});
 
-// store.dispatch({
-//     type: 'CHANGE_SEARCH_TEXT',
-//     searchText: 'Find My Cat'
-// });
+store.dispatch({
+  type: 'CHANGE_SHOW_COMPLETED'
+});
 
-// store.dispatch({
-//     type: 'CHANGE_SEARCH_TEXT',
-//     searchText: 'Find Something Else'
-// });
+store.dispatch({
+  type: 'CHANGE_SHOW_COMPLETED'
+});
