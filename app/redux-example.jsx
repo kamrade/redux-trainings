524f9bea6a859c4a9b50b836698643ea224a3312
Denis Michailov <kamrad.reg@gmail.com>
2017-06-03 11:45:53 +1000

    react-todo-example => redux starts. comments.

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -3,10 +3,22 @@ var redux = require('redux');
 console.log('Starting redux example');
 
 var reducer = (state = { name: 'Anonymous' }, action) => {
-  // state = state || { name: 'Anonymous' };
-
-  return state;
+  switch(action.type) {
+    case 'CHANGE_NAME':
+      return {
+        ...state,
+        name: action.name
+      };
+    default:
+      return state;
+  }
 };
+
 var store = redux.createStore(reducer);
 var currentState = store.getState();
 console.log('currentState', currentState);
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'Denis'
+});
+console.log('Name should be Denis', store.getState());
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -1,15 +1,39 @@
 var redux = require('redux');
-
 console.log('Starting todo redux example');
 
+// +++++++++++++++++++++++++++++++++++++++++++++++
+// initialize state
 var stateDefault = {
   searchText: '',
   showCompleted: false,
   todos: []
 };
-var reducer = (state = stateDefault, action) => {
-  return state;
-};
-var store = redux.createStore(reducer);
 
+// +++++++++++++++++++++++++++++++++++++++++++++++
+// reducer function that control initial state and
+// state changes
+var reducer = (state = stateDefault, action) => {
+  switch(action.type) {
+    case 'SEARCH_TEXT':
+      return {
+        ...state,
+        searchText: action.searchText
+      };
+    default:
+      return state;
+  }
+};
+
+// +++++++++++++++++++++++++++++++++++++++++++++++
+// state declaration
+var store = redux.createStore(reducer);
 console.log( 'current state:', store.getState() );
+
+// +++++++++++++++++++++++++++++++++++++++++++++++
+// first dispatcher
+store.dispatch({
+  type:       'SEARCH_TEXT',
+  searchText: 'Clean the room'
+});
+
+console.log('searchText should be Clean the room', store.getState());
